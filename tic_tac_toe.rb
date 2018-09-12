8ba1c8af9a7162ca925d76f32739598b91da9ae4
Josh Varney <joshcainvarney@gmail.com>
2018-09-12 08:35:47 -0400

    still figuring out the first few tests

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,56 @@
-class Base_class
-	def initialize
-
+class Board
+	def initialize 
+		@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
 	end
+	attr_reader :board
 end
+def game_board(board)
+	board = board
+end	
+p game_board
+
 class Sequential_class
+	attr_reader :board
 	def initialize
-
+		@board = game_board(board)
+		if @board == nil
+			@board = Board.new
+			@board = board.board
+			@board
+		end
+		@board
 	end
+	# def make_move
+	# 	@board.each do |cell|
+	# 		if cell == " "
+	# 			cell
+	# 		end
+	# 	end
+	# end
+	attr_accessor :board		
 end
+stuff = Sequential_class.new
+p stuff.board
 class Random_class
 	def initialize
 
 	end
 end
+
 class Unbeatable_class
 	def initialize
 
 	end
+end
+class Two_player
+	def initialize
+
+	end
+end
+class Player_choice
+	def initialize
+
+	end
+end
+class The_game
 end	
--- tic_tac_toe_test.rb
+++ tic_tac_toe_test.rb
@@ -3,6 +3,13 @@ require_relative 'tic_tac_toe.rb'
 class Tic_tac_toe_test < Minitest::Test
 
 	def test_something
-		assert_equal( , )
+		assert_equal(Array, Board.new.board.class)
+	end
+	def test_that_board_array_contains_nine_elements
+		assert_equal(9, Board.new.board.count)
+	end
+	def test_that_can_use_board_in
+		
+		assert_equal(Array, Sequential_class.new)
 	end
 end	
