d03048cc3967f9cdb7fef0f630ce5403aa366b60
Josh Varney <joshcainvarney@gmail.com>
2018-09-18 13:55:13 -0400

    running a few more tests on cpu

--- tic_tac_toe_test.rb
+++ tic_tac_toe_test.rb
@@ -11,15 +11,21 @@ class Tic_tac_toe_test < Minitest::Test
 	# def test_that_game_board_fills_in_correctly
 	# 	assert_equal([0, 1, 2, 3, 4, 5, 6, 7, 8], Board.new(nil).board_array)
 	# end
-	def test_that_grid_prints_to_console
-		assert_equal(String, Board.new(nil).board.class)
-	end
+	# def test_that_grid_prints_to_console
+	# 	assert_equal(String, Board.new(nil).board.class)
+	# end
 	# def test_that_can_the_game_runs_pvp
 	# 	assert_equal(The_game, The_game.new.class)
 	# end
 	# def test_that_replaces_cells
 	# 	assert_equal(Two_player, Two_player.new.class)
 	# end
+	# def test_that_switches_markers_correctly
+	# 	assert_equal(true, Two_player.new.game_array.include?("O"))
+	# end
+	def test_that_returns_winner_with_three_in_a_row
+		assert_equal("Winner", Winning.new(["X", "X", "X", 3, 4, 5, 6, 7, 8]).check_win
+	end
 	# def test_that_winning_works
 	# 	w = ["X", "X", "X", 3, 4, 5, 6, 7, 8]
 	# 	assert_equal(["X", "X", "X", 3, 4, 5, 6, 7, 8, "Winner"], Winning.new(w))
