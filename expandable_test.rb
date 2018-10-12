17db5bdfc06b1388c9a006e26ba31e02c54d0803
Josh Varney <joshcainvarney@gmail.com>
2018-10-12 10:55:59 -0400

    made test for board array

--- expandable_test.rb
+++ expandable_test.rb
@@ -6,4 +6,7 @@ class Tic_tac_toe_test < Minitest::Test
   def test_that_takes_input_for_board_size
     assert_equal(3, Board.new.size)
   end
+  def test_that_can_make_an_array_for_board_size
+    assert_equal(16, Board.new.board_array.count)
+  end
 end
