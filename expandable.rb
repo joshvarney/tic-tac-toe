cf076f345f7c7d12d37e6f40961b5e3f189cfd7c
Josh Varney <joshcainvarney@gmail.com>
2018-10-12 11:00:43 -0400

    test passed for array size

--- expandable.rb
+++ expandable.rb
@@ -2,6 +2,11 @@ class Board
   def initialize()
     puts "Please enter board size you want minimum is 3:"
     @size = gets.chomp.to_i
+    board_size
   end
+  def board_size
+    @board_array = Array.new(@size * @size)
+  end
+  attr_accessor :board_array
   attr_accessor :size
 end
