de98fd633e7fa23ea52caa2d6cd69b5ea5e1d4d9
Josh Varney <joshcainvarney@gmail.com>
2018-10-15 11:51:08 -0400

    working on how to lay out the board

--- expandable.rb
+++ expandable.rb
@@ -3,11 +3,30 @@ class Board
     puts "Please enter board size you want minimum is 3:"
     @size = gets.chomp.to_i
     board_size
+    draw_board
   end
   def board_size
     @board_array = Array.new(@size * @size).map.with_index{ |x, i| i }
-    p @board_array
+    @board_array
   end
+  def draw_board
+    @grid = ""
+    counter = 0
+    @board_array.each_with_index do |cell, index|
+      @grid << " #{@board_array[index]} "
+        case counter 
+        when 0..@size-2 then grid << "|"
+        when @size-1 then grid << "\n------------\n" unless index == @board_array.count - 1
+      end
+      counter += 1
+      if counter == @size
+        counter = 0
+      end
+    end 
+    @grid
+  end
+  attr_accessor :grid 
   attr_accessor :board_array
   attr_accessor :size
 end
+puts Board.new.grid
